82dd117a7d5b8e2c7c4606d3e6f50f70bb96f1a4
guimarinuchi <127515673+guimarinuchi@users.noreply.github.com>
2024-08-30 20:08:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+export const perguntas = [
+    {
+        enunciado: "Qual das seguintes opções descreve melhor o impacto da globalização nas culturas locais?",
+        alternativas: [
+            {
+                texto: "Globalização!",
+                afirmacao: [
+                    "A globalização promove o intercâmbio cultural, permitindo que elementos de culturas locais sejam compartilhados e integrados com culturas globais, resultando em uma fusão cultural..",
+                    "A globalização tem um impacto neutro nas culturas locais, não influenciando significativamente a preservação ou transformação cultural."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Globalização!",
+                afirmacao: [
+                    "A globalização facilita o intercâmbio cultural e a integração de elementos culturais de diferentes partes do mundo. Isso pode resultar em uma fusão cultural, onde características de culturas locais são misturadas com influências globais, enriquecendo e diversificando as culturas envolvidas.",
+                    "A globalização frequentemente afeta a cultura local, seja promovendo mudanças significativas, seja levando à adaptação e ao intercâmbio cultural. Seu impacto não é neutro, mas sim dinâmico e variado"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+]
